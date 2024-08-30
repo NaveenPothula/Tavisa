6a314ec155596fbf74b242a7e60437a77ce50538
Your Name <you@example.com>
2024-08-30 16:14:24 +0530

    updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,76 +20,76 @@ export default function Navbar({ headerLogo }: NavbarProps) {
   };
 
   return (
-    <nav className="bg-white fixed inset-x-0 z-10 shadow-md px-4 md:px-10">
-      <div className="container ml-0  py-2 flex items-center gap-4 justify-center">
-        <Link href="/" className="flex-shrink-0">
-          <Image
-            src={headerLogo}
-            alt="Logo"
-            width={250}
-            height={80}
-            className="h-16 w-auto"
-          />
-        </Link>
-
-        <div className="hidden md:flex flex-grow flex items-center justify-between mr-0 gap-10">
-          <div className=" md:flex flex-1 items-center justify-between px-0 gap-4">
-            <Link
-              href="/who-are-we"
-              className={`text-center px-2 py-2 text-sm ${
-                pathname === "/who-are-we" ? "text-yellow-500" : "text-gray-800"
-              }`}
-            >
-              Who are we
-            </Link>
-            <Link
-              href="/symptoms"
-              className={`text-center px-2 py-2 text-sm ${
-                pathname === "/symptoms" ? "text-yellow-500" : "text-gray-800"
-              }`}
-            >
-              Symptoms
-            </Link>
-            <Link
-              href="/how-we-treat"
-              className={`text-center px-2 py-2 text-sm ${
-                pathname === "/how-we-treat"
-                  ? "text-yellow-500"
-                  : "text-gray-800"
-              }`}
-            >
-              How we treat
-            </Link>
-            <Link
-              href="/learn"
-              className={`text-center px-2 py-2 text-sm ${
-                pathname === "/learn" ? "text-yellow-500" : "text-gray-800"
-              }`}
-            >
-              Learn
-            </Link>
-          </div>
-          <div className=" hidden md:flex flex-row gap-10 justify-between items-center">
-            <Link
-              href="/shop"
-              className="bg-pink-500 text-white px-2 py-2 rounded-full text-xs hover:bg-blue-600" // Adjusted padding
-            >
-              Shop
-            </Link>
-            <Link
-              href="/#"
-              className="bg-pink-500 text-white px-2 py-2 rounded-full text-sm hover:bg-blue-600" // Adjusted padding
-            >
-              <FaUser />
-            </Link>
-            <Link
-              href="/#"
-              className="bg-pink-500 text-white px-2 py-2 rounded-full text-sm hover:bg-blue-600" // Adjusted padding
-            >
-              <FaShoppingCart />
-            </Link>
-          </div>
+    <nav className="bg-white fixed inset-x-0 z-10 shadow-md px-4 md:px-10 w-full">
+      <div className="ml-0  py-2 flex items-center lg:gap-4 justify-center w-full">
+        <div className="flex-shrink-0">
+          <Link href="/">
+            <Image
+              src={headerLogo}
+              alt="Logo"
+              width={250}
+              height={80}
+              className="h-16 w-auto lg:mr-10 md:mr-4"
+            />
+          </Link>
         </div>
+
+        {/* <div className="hidden md:flex flex-grow flex items-center"> */}
+        <div className="hidden md:flex flex-1 items-center px-0 lg:gap-10">
+          <Link
+            href="/who-are-we"
+            className={`text-center px-2 py-2 text-sm ${
+              pathname === "/who-are-we" ? "text-yellow-500" : "text-gray-800"
+            }`}
+          >
+            Who are we
+          </Link>
+          <Link
+            href="/symptoms"
+            className={`text-center px-2 py-2 text-sm ${
+              pathname === "/symptoms" ? "text-yellow-500" : "text-gray-800"
+            }`}
+          >
+            Symptoms
+          </Link>
+          <Link
+            href="/how-we-treat"
+            className={`text-center px-2 py-2 text-sm ${
+              pathname === "/how-we-treat" ? "text-yellow-500" : "text-gray-800"
+            }`}
+          >
+            How we treat
+          </Link>
+          <Link
+            href="/learn"
+            className={`text-center px-2 py-2 text-sm ${
+              pathname === "/learn" ? "text-yellow-500" : "text-gray-800"
+            }`}
+          >
+            Learn
+          </Link>
+        </div>
+        <div className=" hidden md:flex flex-row lg:gap-10 justify-between items-center gap-4">
+          <Link
+            href="/shop"
+            className="bg-pink-500 text-white px-2 py-2 rounded-full text-xs hover:bg-blue-600" // Adjusted padding
+          >
+            Shop
+          </Link>
+          <Link
+            href="/#"
+            className="bg-pink-500 text-white px-2 py-2 rounded-full text-sm hover:bg-blue-600" // Adjusted padding
+          >
+            <FaUser />
+          </Link>
+          <Link
+            href="/#"
+            className="bg-pink-500 text-white px-2 py-2 rounded-full text-sm hover:bg-blue-600" // Adjusted padding
+          >
+            <FaShoppingCart />
+          </Link>
+        </div>
+        {/* </div> */}
         <button
           onClick={toggleMenu}
           className="md:hidden text-gray-800 ml-auto"
